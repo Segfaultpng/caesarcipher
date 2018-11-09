f25f81602dcb59545f72dea25870ebc8b040642c
Stephen P <stephenp@ZipcodersMBP79.fios-router.home>
2018-11-09 00:36:23 -0500

    init

---
+++ iocipher.js
@@ -0,0 +1,12 @@
+var iocipher = function(){
+
+    var str = prompt("Enter in your string");
+    var num = prompt("Enter in your number");
+    //code here
+
+
+}
+
+window.onload = function () {
+iocipher();
+}
